16bbda4dc587c21542c003aa8d8a001293b5c44a
istiakAHMEDsaad <istik2020@gmail.com>
2024-11-03 19:48:37 +0600

    done dash board part with rechart

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,25 +1,72 @@
-// import { useEffect, useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
-// import Graph from './Graph';
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Cell,
+  ResponsiveContainer,
+  Tooltip,
+} from 'recharts';
 
 const Dashboard = () => {
   const data = useLoaderData();
-  /* const [data, setData] = useState();
-  useEffect(() => {
-    fetch('/booksData.json').then((res) =>
-      res
-        .json()
-        .then((data) => setData(data))
-        .catch((err) => console.error(err))
-    );
-  }, []);
-  console.log(data.bookId) */
-  console.log(data)
+  const colors = [
+    '#0088FE',
+    '#00C49F',
+    '#FFBB28',
+    '#FF8042',
+    'red',
+    'pink',
+    '#8a2be2',
+    '#420420',
+    '#00ff7f',
+  ];
+
+  const getPath = (x, y, width, height) => {
+    return `M${x},${y + height}C${x + width / 3},${y + height} ${
+      x + width / 2
+    },${y + height / 3}
+    ${x + width / 2}, ${y}
+    C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${
+      x + width
+    }, ${y + height}
+    Z`;
+  };
+
+  const TriangleBar = (props) => {
+    const { fill, x, y, width, height } = props;
+
+    return <path d={getPath(x, y, width, height)} stroke='none' fill={fill} />;
+  };
 
   return (
     <div className='flex items-center justify-center'>
-      <div>
-        {/* {data.map((para, index) => <Graph key={index} para={para}></Graph>)} */}
+      <div className='w-full max-w-4xl'>
+        <ResponsiveContainer width='100%' height={500}>
+          <BarChart
+            height={500}
+            width={800}
+            data={data}
+            margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
+          >
+            <CartesianGrid strokeDasharray='3 3'></CartesianGrid>
+            <XAxis dataKey='bookName' />
+            <YAxis></YAxis>
+            <Tooltip></Tooltip>
+            <Bar
+              dataKey='totalPages'
+              fill='#8884d8'
+              shape={<TriangleBar />}
+              label={{ position: 'top' }}
+            >
+              {data.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+              ))}
+            </Bar>
+          </BarChart>
+        </ResponsiveContainer>
       </div>
     </div>
   );
